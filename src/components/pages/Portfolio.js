4f0ec408cc4c9a2530d88bb45c992d31c0120166
gato365 <immanuelwilliams@gato365.com>
2023-02-22 07:07:25 -0800

    Added details to portfoflio page

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+
+// Function to display the portfolio page in the browser
+// Allow for 4 projects to be displayed which all have a title, image, and description
+// The image will be a link to the deployed application
+// Allow for Hover effect on the image to display the title of the project
+// Allow for the title to be a link to the deployed application
+
+function Portfolio() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-12">
+                    <h1>Portfolio</h1>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <h2>Project 1</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <a href=""><img src="" alt="Project 1" /></a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <p>Project 1 Description</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <h2>Project 2</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <a href=""><img src="" alt="Project 2" /></a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <p>Project 2 Description</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <h2>Project 3</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <a href=""><img src="" alt="Project 3" /></a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <p>Project 3 Description</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <h2>Project 4</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <a href=""><img src="" alt="Project 4" /></a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12">
+                    <p>Project 4 Description</p>
+                </div>
+            </div>
