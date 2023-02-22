5baea263707f1e2b472c3cfb9249ae130522c321
gato365 <immanuelwilliams@gato365.com>
2023-02-22 07:58:54 -0800

    Made appropraite changes to page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  return 'hello world!';
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
