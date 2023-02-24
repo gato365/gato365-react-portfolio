1b972ae5f486237f6b27fce48cd420cf6a834517
gato365 <immanuelwilliams@gato365.com>
2023-02-24 08:23:21 -0800

    Made modifications to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,18 +4,12 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
-      </p>
+     
+  
+ <p>Dr. Williams is dedicated to changing the lives of all his students by initially believing in their ability to learn data science, machine learning, and statistics. He is also the chief curriculum developer for the math worksheets found in GATO365 Math Boot Camp. He spends most of his time developing and designing curricula for all audiences so that every student has the opportunity to learn complicated concepts. Dr. Williams has a Ph.D. in psychometrics while earning a master's in statistics and mathematics. He is currently earning his full stack development certificate from University of California Berkeley.</p>
+
+<p>Dr. Williams has a philosophy centered around confidence and discipline. He began his journey by creating a math book to help students be curious in the math classroom. He then started to provide Boys and Girls Clubs math worksheets in 6 different states! Dr. Williams thought it made more senses to teach data science to high school students since that is an up and coming field and there is always a need for passionate teachers.</p>
+      
     </div>
   );
 }
